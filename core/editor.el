6fa8017486b11077c6f9ad3480c03ad0bacbdbf5
Knuth <kamase79@daum.net>
2015-08-31 05:27:40 +0900

    first emacs environment

---
+++ core/editor.el
@@ -0,0 +1,156 @@
+;;============================================================
+;;                         Coding
+;;============================================================
+;; default tab
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4)
+
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+
+;; source folding
+(global-set-key (kbd "C-b") 'hs-hide-block)
+(global-set-key (kbd "C-s") 'hs-show-block)
+
+;; smart tab behavior - indent or complete
+(setq tab-always-indent 'complete)
+
+;; gdb windows option.
+(setq gdb-many-windows t)
+
+;;;;
+;;;; Source Navigator cedet
+;;;;
+
+(require 'cc-mode)
+(require 'semantic)
+
+;; enabled features will depend on semantic-mode 1
+(semantic-mode 1)	
+(setq cedet-global-command "global")
+
+;; enables global support for semanticdb
+(global-semanticdb-minor-mode 1)	
+(global-semantic-mru-bookmark-mode 1)
+(global-semantic-highlight-func-mode 1)
+(global-semantic-stickyfunc-mode 1)
+(global-semantic-decoration-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+(global-semantic-idle-local-symbol-highlight-mode 1)
+(global-semantic-idle-completions-mode 1)
+(global-semantic-idle-summary-mode 1)
+(global-semantic-show-unmatched-syntax-mode 1)
+(global-semantic-show-parser-state-mode 1)
+(global-semantic-highlight-edits-mode 1)
+
+;; To enable more advanced functionality for name completion, etc.,
+;; You can load the semantic/ia with following command:
+(require 'semantic/ia)
+
+;; If you're using GCC for programming in C & C++, then Semantic can
+;; automatically find directory,
+;;where system include files are stored.
+(require 'semantic/bovine/gcc)
+
+;; Enable case-insensitive searching
+(set-default 'semantic-case-fold t)
+
+;;============================================================
+;;                          UI
+;;============================================================
+;;;; Editor's Color changing
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+(set-face-foreground 'ac-candidate-face "blue4")
+(set-face-background 'ac-candidate-face "DodgerBlue1")
+(set-face-foreground 'ac-selection-face "white")
+(set-face-background 'ac-selection-face "steelblue")
+(set-face-foreground 'ac-completion-face "white")
+(set-face-background 'ac-completion-face "black")
+
+;;;; powerline
+(setq powerline-default-theme t)
+;;============================================================
+;;                         EDIT
+;;============================================================
+;; Coding System Read File encoding.
+(modify-coding-system-alist 'file "\\.c\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.py\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.S\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.h\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.txt\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.cpp\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.txt\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.md\\'" 'utf-8)
+
+;; Loding Korean Font Set
+(set-fontset-font "fontset-default" '(#x1100 . #xffdc)
+                   '("Gulim" . "iso10646-1"))
+(set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
+                   '("Gulim" . "iso10646-1"))
+
+(setq face-font-rescale-alist
+       '((".*hiragino.*" . 0.9)
+	 (".*Gulim.*" . 0.9)))
+(set-language-environment "Korean")
+
+(setq coding-system-for-read 'utf-8)
+
+;;;;
+;;;; Cedet semanticdb
+;;;;
+;; if you wnat to enable support for gnu global
+(setq cedet-global-command "global")
+;;(when (cedet-gnu-global-version-check t)) ;; is not active
+(semanticdb-enable-gnu-global-databases 'c-mode t)
+(semanticdb-enable-gnu-global-databases 'c++-mode t)
+(setq-default semantic-symref-tool "global")
+
+;;;;
+;;;; TAGS Table on linux kernel ( ~/linux_kernel/linux-3.12.20 )
+;;;;
+(setq tags-table-list '("/home/knuth/linux_kernel/linux-3.12.20"))
+
+;;;;
+;;;; Helm Gtags Settings
+;;;;
+(require 'helm-config)
+(require 'helm-gtags)
+
+;; Enable helm-gtags-mode
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+(add-hook 'php-mode-hook 'helm-gtags-mode)
+
+;; helm gtags customize
+(setq helm-gtags-path-style 'relative)
+(setq helm-gtags-ignore-case t)
+(setq helm-gtags-auto-update t)
+
+;;;;
+;;;; setup compile package
+;;;;
+(require 'compile)
+(setq compilation-disable-input nil)
+(setq compilation-scroll-output t)
+(setq mode-compile-always-save-buffer-p t)
+
+;;;;
+;;;; ECB 
+;;;;
+(setq ecb-version-check nil)  ; to prevent ecb failing to start up
+(setq-default ecb-tip-of-the-day nil)
+
+;;;;
+;;;; smex 
+;;;;
+;; like ido frequently used commands.
+(autoload 'smex "smex"
+  "Smex is a M-x enhancement for Emacs, it provides a convenient interface to
+your recently and most frequently used commands.")
+
+(global-set-key (kbd "M-x") 'smex)
+
+(provide 'editor)
---
+++ melpa/hl-todo-1.4.4/hl-todo-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hl-todo" "1.4.4" "highlight TODO keywords" 'nil :url "http://github.com/tarsius/hl-todo" :keywords '("convenience"))
