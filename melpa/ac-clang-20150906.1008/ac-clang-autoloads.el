6f8696e1988dd17ed915cde85559a5ae35f6a6ae
douden79 <kamase79@daum.net>
2016-01-18 01:31:36 +0900

    add

---
+++ melpa/ac-clang-20150906.1008/ac-clang-autoloads.el
@@ -0,0 +1,16 @@
+;;; ac-clang-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ac-clang-pkg.el" "ac-clang.el") (22171
+;;;;;;  47750 332830 517000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-clang-autoloads.el ends here
--- melpa/org-outlook-0.11/org-outlook-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "org-outlook" "0.11" "Outlook org" 'nil :url "https://github.com/mlf176f2/org-outlook.el" :keywords
-  '("org-outlook"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
